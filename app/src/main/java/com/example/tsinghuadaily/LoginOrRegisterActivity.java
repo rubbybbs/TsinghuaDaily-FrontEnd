436c63df52b329c74eba0240d9a82d78b2dd66af
rubbybbs <rubbybbs1324@gmail.com>
2020-05-15 21:55:42 +0800

    开始界面

---
+++ app/src/main/java/com/example/tsinghuadaily/LoginOrRegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.tsinghuadaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
+
+public class LoginOrRegisterActivity extends AppCompatActivity {
+
+    private QMUIRoundButton LoginBtn, RegisterBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_register);
+
+        // 设置按钮点击效果
+        LoginBtn = (QMUIRoundButton)findViewById(R.id.LoginBtn);
+        RegisterBtn = (QMUIRoundButton)findViewById(R.id.RegisterBtn);
+
+        LoginBtn.setChangeAlphaWhenPress(true);
+        RegisterBtn.setChangeAlphaWhenPress(true);
+    }
+
+
+
+}
