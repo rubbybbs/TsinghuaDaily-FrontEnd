16626eb3d1e1691a1036016d680c45a6af8c1f33
RUBBY <hgd17@mails.tsinghua.edu.cn>
2020-05-29 18:10:09 +0800

    完成基本界面跳转逻辑

--- app/src/main/java/com/example/tsinghuadaily/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/tsinghuadaily/Activity/RegisterActivity.java
@@ -8,30 +8,41 @@ import android.view.View;
 import com.example.tsinghuadaily.R;
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 import com.qmuiteam.qmui.widget.QMUITopBar;
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     QMUITopBar topBar;
+    QMUIRoundButton getVerifyBtn;
+    QMUIRoundButton registerBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         topBar = (QMUITopBar) findViewById(R.id.topbar);
-
-        //QMUIStatusBarHelper.translucent(this);
-
+        getVerifyBtn = (QMUIRoundButton) findViewById(R.id.button_getverifycode);
+        registerBtn = (QMUIRoundButton) findViewById(R.id.button_register);
+        getVerifyBtn.setChangeAlphaWhenPress(true);
+        registerBtn.setChangeAlphaWhenPress(true);
+        registerBtn.setOnClickListener(this);
         initTopBar();
 
     }
 
     private void initTopBar() {
-        topBar.addLeftBackImageButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        topBar.addLeftBackImageButton().setOnClickListener(v -> finish());
         topBar.setTitle("注册账号");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_getverifycode) {
+
+        }
+        else if (v.getId() == R.id.button_register) {
+            //TODO: 发送注册请求，获取状态并用对话框提示。
+            finish();
+        }
     }
 }
